278a1e479bc35c67fd2bf3abc50102d81514d6cb
prabhakararao.nandigrama <prabhakararao.nandigrama@assettl.com>
2024-09-14 12:19:04 +0530

    update

--- variables/variable.tf
+++ variables/variable.tf
@@ -1,14 +1,15 @@
 variable "ami_id" {
   type        = string
   default     = "ami-09c813fb71547fc4f"
-  description = "This is the AMI ID of devops-practice which is RHEL-9"
+  description = "This is the AMI ID of devops-practice RHEL-9"
 }
 variable "instance_type" {
     type = string
     default = "t3.micro"
 }
+
 variable "tags" {
-    type = map 
+    type = map #optional
     default = {
         Name = "backend"
         Project = "expense"
@@ -16,4 +17,31 @@ variable "tags" {
         Environment = "DEV"
         Terraform = "true"
     }
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "Allow port number 22 for SSH access"
+}
+
+variable "from_port" {
+    default = 22
+    type = number
+}
+
+variable "to_port" {
+    default = 22
+    type = number
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "ingress_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
 }
